c3b2a29f5745deb9a45c1cf1ead8ea9f7d96f051
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-15 14:14:35 -0700

    Add attributes test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,5 +1,12 @@
 class NightReader
+  attr_accessor :read_file,
+                :write_file
+                
   def initialize
-    
+    @read_file = ARGV[1]
+    @write_file = ARGV[0]
   end
 end
+
+# night_reader = NightReader.new
+# night_reader.call
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe NightReader do
     it "exists" do
       expect(night_reader).to be_a(NightReader)
     end
+
+    it "has attributes" do
+      night_reader.call
+
+      expect(night_reader.read_file).to eq('./braille.txt')
+      expect(night_reader.write_file).to eq('./message.txt')
+    end
+    end
   end
 end
